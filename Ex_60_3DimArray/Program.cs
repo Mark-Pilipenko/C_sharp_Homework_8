381aebbacac5f38b19aa8ee7513a6b09008990aa
Mark <kalmark@mail.ru>
2022-11-15 17:21:08 +0300

    Edited

--- Ex_60_3DimArray/Program.cs
+++ Ex_60_3DimArray/Program.cs
@@ -72,17 +72,3 @@ void PrintArray3(int[,,] matrix)
         }
     }
 }
-
-// void PrintArray3(int[,,] matrix)
-// {
-//     for (int i = 0; i < matrix.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < matrix.GetLength(1); j++)
-//         {
-//             for (int k = 0; k < matrix.GetLength(2); k++)
-//             {
-//                 Console.Write($"{matrix[i, j, k]}({i},{j},{k}) ");
-//             }
-//         }
-//     }
-// }
